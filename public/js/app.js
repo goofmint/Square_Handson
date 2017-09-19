37dfa8c43892c933e32861de89612047f44537b8
YEbihara <yuuki.ebihara813@gmail.com>
2017-09-19 20:50:45 +0900

    HTML form created

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+// 定数の定義
+var url = 'https://cryptic-oasis-18035.herokuapp.com/'; // コールバックするURL
+var client_id = 'sq0idp-6RKMWthYz8P4CnavGgHGZw'; // SquareのアプリケーションID
+
+// Onsen UIが使える状態になったところから処理開始
+ons.ready(function() {
+  // ボタンを押した時のイベント処理
+    // 変数の取得
+
+    // パラメータの生成
+
+    // URLの生成
+
+    // POSレジアプリ呼び出し
+});
